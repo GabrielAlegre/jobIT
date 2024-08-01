c97ff7ce8fe72bf7e78fb4ad23dc54cb4028584c
GabrielAlegre <gabrielalegre68@gmail.com>
2024-08-01 15:54:24 -0300

    creación de componentes inicio admin y empleado

---
+++ src/app/core/components/homes/inicio-empleado/inicio-empleado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'empleado-inicio-empleado',
+  standalone: true,
+  imports: [],
+  templateUrl: './inicio-empleado.component.html',
+  styleUrl: './inicio-empleado.component.scss'
+})
+export class InicioEmpleadoComponent {
+
+}
